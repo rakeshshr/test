a9d63f09465fca5e35b0399bdb90422ca24499af
rakesh shrestha <rakeshmac@Rakeshmacbook-air.local>
2015-07-20 15:58:44 -0700

    save

--- models/log.js
+++ models/log.js
@@ -14,51 +14,58 @@ module.exports = Log;
 
 
 /*
-({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
+({project_name: "Events Around Us",
+image:("https://upload.wikimedia.org/wikipedia/commons/0/0f/Hearst_Greek_Theatre_(Berkeley,_CA).JPG"),
+ Venue: "Greek Theatre",
+  address:"2001 Gayley Rd",
+   city_state: "Berkeley, California",
+    zip: "94720",
+     phone: "(510-548-3010"})
+
+     ({project_name: "Events Around Us",
+image: ("http://localmusicvibe.com/sites/default/files/images/venues/fs-graphic.jpg"),
+ Venue: "Freight & Salvage Coffeehouse",
+  address:"2020 Addison st",
+   city_state: "Berkeley, California",
+    zip: "94530",
+     phone: "(510)-644-2020"})
+
+     ({project_name: "Events Around Us",
+image: ("http://media-cdn.tripadvisor.com/media/photo-s/02/a8/c1/50/zachary-s-chicago-pizza.jpg"),
+ Venue: "Zachary's Chicago Pizza",
+  address:"2289 Fresno Ave",
+   city_state: "Berkeley, California",
     zip: "94606",
+     phone: "(510)-525-1389"})
+
+     ({project_name: "Events Around Us",
+image: ("http://s3-media4.fl.yelpcdn.com/bphoto/sx_DH-0hPHX2JLooJHmB_g/o.jpg"),     
+ Venue: "Pappys's Grill & Bar",
+  address:"2289 International Boulevard",
+   city_state: "Albany, California",
+    zip: "94609",
      phone: "(510)-575-1359"})
 
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
-    zip: "94606",
-     phone: "(510)-575-1359"})
+     ({project_name: "Event Around Us",
+image: ("http://fuzzybrew.com/wp-content/uploads/2011/04/jupiter-sign.jpg"),     
+ Venue: "Jupiter",
+  address:"2181 Shattuck Ave",
+   city_state: "berkeley, California",
+    zip: "94530",
+     phone: "(510)-843-8277"})
 
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
+     ({project_name: "Event Around Us",
+image: ("http://www.berkeleyside.com/wp-content/uploads/2014/05/Screen-shot-2014-05-16-at-12.05.11-PM-720x470.png"),     
+ Venue: "Townie",
+  address:"1799 University Ave",
+   city_state: "Berkely, California",
     zip: "94606",
-     phone: "(510)-575-1359"})
+     phone: "(510)-356-4903"})
 
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
-    zip: "94606",
-     phone: "(510)-575-1359"})
-
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
-    zip: "94606",
-     phone: "(510)-575-1359"})
-
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
-   city_state: "Oakland, California",
-    zip: "94606",
-     phone: "(510)-575-1359"})
-
-     ({project_name: "ROOTS: Reclaiming Our Traditions and Stories",
- organization: "Communities United for Restorative Youth Justice",
-  contact:"Ruben Leal", address:"2289 International Boulevard",
+     ({project_name: "Event Around Us",
+image: ("https://manhattanwithatwistdotcom.files.wordpress.com/2013/09/fotofilip-hl-graceinteriorportraits-2851.jpg"),     
+ Venue: "kips",
+  address:"2289 Telegraph Ave",
    city_state: "Oakland, California",
     zip: "94606",
      phone: "(510)-575-1359"})
--- server.js
+++ server.js
@@ -6,11 +6,7 @@ var express = require('express'),
     User = require('./models/user'),
     session = require('express-session');
 
-    mongoose.connect(
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/fun-around-us' // plug in the db name you've been using
-);
+    
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/gigs_around_me');
@@ -25,100 +21,6 @@ app.use(session({
   secret: 'SuperSecretCookie',
   cookie: { maxAge: 60000 }
 }));
-
-// middleware to manage sessions
-app.use('/', function (req, res, next) {
-  // saves userId in session for logged-in user
-  req.login = function (user) {
-    req// require mongoose
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-// define log schema
-var LogSchema = new Schema({
-  type: String,
-  calories: Number
-});
-
-// create and export Log model
-var Log = mongoose.model('Log', LogSchema);
-module.exports = Log;user currently logged in based on `session.userId`
-  req.currentUser = function (callback) {
-    User.findOne({_id: req.session.userId}, function (err, user) {
-      req.user = user;
-      callback(null, user);
-    });
-  };
-
-  // destroy `session.userId` to log out user
-  req.logout = function () {
-    req.session.userId = null;
-    req.user = null;
-  };
-
-  next();
-});
-
-// STATIC ROUTES
-app.use(express.static(__dirname + '/public'));
-
-// root route
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/views/index.html');
-});
-
-// USER ROUTES
-
-// create new user with secure password
-app.post('/users', function (req, res) {
-  var newUser = req.body.user;
-  User.createSecure(newUser, function (err, user) {
-    // log in user immediately when created
-    req.login(user);
-    res.redirect('/profile');
-  });
-});
-
-// authenticate user and set session
-app.post('/login', function (req, res) {
-  var userData = req.body.user;
-  User.authenticate(userData.email, userData.password, function (err, user) {
-    req.login(user);
-    res.redirect('/profile');
-  });
-});
-
-// profile page
-app.get('/profile', function (req, res) {
-  req.currentUser(function (err, user) {
-    res.sendFile(__dirname + '/public/views/profile.html');
-  });
-});
-
-// API ROUTES
-
-// show all logs
-app.get('/api/logs', function (req, res) {
-  Log.find(function (err, logs) {
-    res.json(logs);
-  });
-});
-
-// create new log
-app.post('/api/logs', function (req, res) {
-  // create new instance of Log
-  var newLog = new Log({
-    type: req.body.type,
-    calories: req.body.calories
-  });
-
-  // save new log in db
-  newLog.save(function (err, savedLog) {
-    res.json(savedLog);
-  });
-});
-
-
 
 
 
